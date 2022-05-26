9e2fc27d108aa227df6f27ecb18dc8dc5226c2fc
tracer-ics2021 <tracer@njuics.org>
2022-05-26 14:13:44 +0800

    >  compile Y30200959

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -6,7 +6,7 @@ const char *regs[] = {"$0", "ra", "sp", "gp", "tp", "t0", "t1", "t2", "s0", "s1"
 
 void isa_reg_display() {
     for (int i = 0; i < 32; i++) {
-        printf("%s\n", regs[i]);
+        printf("%s \t%x \t%d\n", regs[i], cpu.gpr[i]._32, cpu.gpr[i]._32);
     }
 }
 
